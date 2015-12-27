f461b6e66b8233b7a6be26d730b1b5a863fa0dad
alex <alex@k3wl.net>
2015-12-27 20:13:49 +0100

    Änderungen WebNav

--- reach.k3wl.net/views/webnav.php
+++ reach.k3wl.net/views/webnav.php
@@ -1,3 +1,13 @@
-
-:: <a href="<?= $app->urlFor('logout');?>">Logout</a>
-:: 
+<div class="row-fluid">
+  <div class="span2">&nbsp;</div>
+  <div class="span8">
+    <ul class="nav nav-pills">
+      <li class="active">
+        <a href="#"><i class="icon-home"></i></a>
+      </li>
+      <li><a href="#">...</a></li>
+      <li><a href="<?= $app->urlFor('logout');?>"><i class="icon-remove"></i></a></li>
+    </ul>
+  </div>
+  <div class="span2">&nbsp;</div>
+</div>
