ae91770e5001517944c1deabf1c5f4c4a4f3a04d
alex <alex@k3wl.net>
2016-01-01 10:14:56 +0100

    Rebuild Nav for Login.php

--- reach.k3wl.net/views/login.php
+++ reach.k3wl.net/views/login.php
@@ -6,6 +6,12 @@ $app->render('webheader.php', array(
 
 <div class="container">
 
+  <?php
+  $app->render('webnav.php', array(
+        'app' => $app,
+        'site' => __FILE__
+  ));?>
+
   <form class="form-signin"  method="post" action="<?=$app->urlFor('login');?>">
     <h2 class="form-signin-heading">Please sign in</h2>
     <label for="inputUser" class="sr-only">User Name</label>
@@ -13,7 +19,7 @@ $app->render('webheader.php', array(
 
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="inputPassword" class="form-control" placeholder="Password"name="pass" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Start now</button>
   </form>
 
 </div> <!-- /container -->
