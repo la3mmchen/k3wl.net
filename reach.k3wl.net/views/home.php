202a054ceee9fa7f3e710864643c4639c0ad4a75
alex <alex@k3wl.net>
2016-01-02 19:39:34 +0100

    Worked at the teaser.

--- reach.k3wl.net/views/home.php
+++ reach.k3wl.net/views/home.php
@@ -16,11 +16,13 @@ $app->render('webheader.php', array(
 
 
   <div class="jumbotron">
-          <h1>Hej du.</h1>
+          <h1>Hej.</h1>
           <p class="lead">
-            This is meant as a small playground.
+            What's App, Facebook, Mobile, Business mobile, Twitter, etc. We collect lot of communication channels these days. But how do you signalize you fancy a mail over all other channels while you're sitting at hair dresser? How do you let the world know that at the moment you just want a chat message instead of a call? <br/>
+            So far there is no way. This might be one solution. So give it a try.
           </p>
     </div>
+    <div class="alert alert-info" role="alert">You might notive we are still very beta. So give a hint, there is a reach.k3wl.net user to show you how do you can contact us. Otherwise drop us a mail.</div>
   <?php #stop changing ?>
 </div>
 <?php
