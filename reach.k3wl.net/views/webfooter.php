2e97bf7736cfaa77ad08a5582322a850ff1aa7b7
alex <alex@k3wl.net>
2016-01-03 11:31:57 +0100

    fixed bootstrap.js integration

--- reach.k3wl.net/views/webfooter.php
+++ reach.k3wl.net/views/webfooter.php
@@ -1,5 +1,5 @@
   <br/><br/>
-  
+
   <footer class="footer">
       <div class="container">
         <p class="text-muted text-center"> <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> site ( at ) <?=$app->getName();?> <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> <a href="<?=$app->urlFor('user', array('username'=>$app->getName()));?>">see our profile</a> </p>
@@ -7,5 +7,6 @@
   </footer>
   <script src="http://code.jquery.com/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="http://<?=$_SERVER['HTTP_HOST'];?><?=$_SERVER['SCRIPT_NAME'];?>/../js/bootstrap.min.js"></script>
   </body>
 </html>
